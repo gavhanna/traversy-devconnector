1d3e0d30382076afa2ad15d41730f931c8bef8a5
Gav Hanna <gavhanna@gmail.com>
2018-08-25 16:07:29 +0100

    json web token auth working

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://gavhanna:gav123@ds125302.mlab.com:25302/traversy-devconnector"
+  mongoURI: "mongodb://gavhanna:gav123@ds125302.mlab.com:25302/traversy-devconnector",
+  secretOrKey: "some random string of whatever, maybe change this later?"
 };
